26b23c86c00c107bcc1524a593b6b2228bfa248e
wanglixing <wanglixing@xiaomi.com>
2018-01-02 15:34:21 +0800

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 /* eslint-disable */
 import 'lib-flexible/flexible.js'
+import 'normalize.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
