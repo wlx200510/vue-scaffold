b47178de9154c82351b5c529d1772709b63520a3
wanglixing <wanglixing@xiaomi.com>
2018-01-02 15:34:21 +0800

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'normalize.css'
 import Vue from 'vue'
 import {Button, Select} from 'element-ui'
 import App from './App'
